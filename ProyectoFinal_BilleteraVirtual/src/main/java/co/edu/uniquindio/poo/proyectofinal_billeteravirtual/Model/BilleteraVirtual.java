703c26711ea960206784ecd623d9a09a348253fa
Camilo2B <camilo.ruizl@uqvirtual.edu.co>
2025-05-10 19:12:42 -0500

    Delete ProyectoFinal_BilleteraVirtual directory

--- ProyectoFinal_BilleteraVirtual/src/main/java/co/edu/uniquindio/poo/proyectofinal_billeteravirtual/Model/BilleteraVirtual.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo.proyectofinal_billeteravirtual.Model;
-
-public interface BilleteraVirtual {
-
-    public void IniciarSesion();
-
-    public void CrearCuenta();
-}
--- ProyectoFinal_BilleteraVirtual/src/main/java/co/edu/uniquindio/poo/proyectofinal_billeteravirtual/Model/DepositoFactory.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo.proyectofinal_billeteravirtual.Model;
-
-public class DepositoFactory extends TransaccionFactory{
-
-    @Override
-    public void CrearTransaccion() {
-        System.out.println("Deposito Creado");
-    }
-}
