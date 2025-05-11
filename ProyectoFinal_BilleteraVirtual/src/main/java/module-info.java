1ce4ef09ab2e7572fb9d406eafa2f00d4521f7a0
Camilo2B <camilo.ruizl@uqvirtual.edu.co>
2025-05-10 19:04:55 -0500

    visuales2

--- ProyectoFinal_BilleteraVirtual/src/main/java/module-info.java
+++ ProyectoFinal_BilleteraVirtual/src/main/java/module-info.java
@@ -2,9 +2,6 @@ module co.edu.uniquindio.poo.proyectofinal_billeteravirtual {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens co.edu.uniquindio.poo.proyectofinal_billeteravirtual to javafx.fxml;
+    opens co.edu.uniquindio.poo.proyectofinal_billeteravirtual.ViewController to javafx.fxml;
     exports co.edu.uniquindio.poo.proyectofinal_billeteravirtual;
-    exports co.edu.uniquindio.poo.proyectofinal_billeteravirtual.Model;
-    opens co.edu.uniquindio.poo.proyectofinal_billeteravirtual.Model to javafx.fxml;
 }
