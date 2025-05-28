8284c86b890aeee3aaef5b11b7e65bf72ebe8d68
Camilo2B <camilo.ruizl@uqvirtual.edu.co>
2025-05-27 19:39:35 -0500

    Delete ProyectoFinal_BilleteraVirtual directory

--- ProyectoFinal_BilleteraVirtual/src/main/java/co/edu/uniquindio/poo/proyectofinal_billeteravirtual/Model/ICuenta.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo.proyectofinal_billeteravirtual.Model;
-
-public interface ICuenta {
-
-    public void CrearCuenta();
-    public void ModificarCuenta();
-    public void EliminarCuenta();
-}
